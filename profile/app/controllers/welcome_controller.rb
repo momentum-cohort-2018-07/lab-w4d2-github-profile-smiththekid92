fa798eac9412e6c59b6c43667c33fd196e91825a
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-08-21 15:20:18 -0400

    Updated page to display repo info. Adding additional data.

--- profile/app/controllers/welcome_controller.rb
+++ profile/app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
+  # include HTTParty
   def index
+    auth = {:username => "smiththekid92", :password => "d39d9764b5c0bc74dfb29a5a1c84dfea8776559b"}
+    @profile = HTTParty.get('https://api.github.com/users/smiththekid92', :basic_auth => auth)
+    @repo = HTTParty.get('https://api.github.com/users/smiththekid92/repos', :basic_auth => auth)
   end
 end
